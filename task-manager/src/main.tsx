cd80606bf55141e31c1e884042afd592c8a24cc9
Gerald Okafor <zobaimpact@gmail.com>
2025-02-12 12:12:05 +0100

    setup test using jest

--- task-manager/src/main.tsx
+++ task-manager/src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store/configureStore.ts";
+import { store} from "./redux/store/configureStore.ts";
 import App from './App.tsx'
 
 
